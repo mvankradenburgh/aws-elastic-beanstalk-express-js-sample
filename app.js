2cf7530f9a7b1d1584306b772a24ca30aa581c13
Mike Van Kradenburgh <mvankradenburgh@gmail.com>
2021-02-05 23:43:35 -0500

    added new line

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello to my World!'));
+app.get('/', (req, res) => res.send('Tested through full AWS pipeline.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
