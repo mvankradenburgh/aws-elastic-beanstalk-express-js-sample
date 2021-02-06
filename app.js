6af7b33875400778f5a01c29db1e252aed80f3c3
Mike Van Kradenburgh <mvankradenburgh@gmail.com>
2021-02-05 23:56:45 -0500

    Updated second message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello to my World!'));
-app.get('/', (req, res) => res.send('Tested through full AWS pipeline.'));
+app.get('/', (req, res) => res.send('Tested through full AWS Code-pipeline.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
