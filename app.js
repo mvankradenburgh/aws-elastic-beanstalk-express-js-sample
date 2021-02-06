bf72a9a2aabe5bfce09e774a3752e0cf11452741
Mike Van Kradenburgh <mvankradenburgh@gmail.com>
2021-02-06 00:06:48 -0500

    updated main message

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello to my World!'));
-app.get('/', (req, res) => res.send('Tested through full AWS Code-pipeline.'));
+app.get('/', (req, res) => res.send('Hello to my World! - Deployed with AWS Code-Pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
